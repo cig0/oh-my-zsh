e3dcad7e3d017eba9580ab8fbdb08923597a105e
Martin Cigorraga <tincho@tutanota.com>
2020-12-29 00:01:53 -0300

    Add lazy completion versions for kubectl, helm and minikube

---
+++ custom/plugins/helm-lazy/helm-lazy.plugin.zsh
@@ -0,0 +1,20 @@
+# Autocompletion for helm.
+#
+if (( $+commands[helm] )); then
+    __HELM_COMPLETION_FILE="${ZSH_CACHE_DIR}/helm_completion"
+
+    if [[ ! -f $__HELM_COMPLETION_FILE ]]; then
+        helm completion zsh >! $__HELM_COMPLETION_FILE
+    fi
+
+    lazy-load() {
+
+      unfunction "$0"
+
+      [[ -f $__HELM_COMPLETION_FILE ]] && source $__HELM_COMPLETION_FILE
+
+      unset __HELM_COMPLETION_FILE
+
+    }
+
+fi
---
+++ custom/plugins/minikube-lazy/minikube-lazy.plugin.zsh
@@ -0,0 +1,20 @@
+# Autocompletion for Minikube.
+#
+if (( $+commands[minikube] )); then
+    __MINIKUBE_COMPLETION_FILE="${ZSH_CACHE_DIR}/minikube_completion"
+
+    if [[ ! -f $__MINIKUBE_COMPLETION_FILE ]]; then
+        minikube completion zsh >! $__MINIKUBE_COMPLETION_FILE
+    fi
+
+    lazy-load() {
+
+      unfunction "$0"
+
+      [[ -f $__MINIKUBE_COMPLETION_FILE ]] && source $__MINIKUBE_COMPLETION_FILE
+
+      unset __MINIKUBE_COMPLETION_FILE
+
+    }
+
+fi
